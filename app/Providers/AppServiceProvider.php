8ca3b1b82928cd6c7e32d65a91c0ac55af73be35
Yunus28208 <yunussyahrul2810@gmail.com>
2025-05-25 16:58:37 +0800

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\FacadesURL
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,11 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        gate::define('view-student', function (user $user) {
-            if ($user->role == 'admin' || $user->role === 'guest') {
-                return true;
-            }
-            return false;
+        if (env('APP_ENV') === 'production') {
+         URL::forceScheme('https');
+    }
+
         });
 
         Gate::define('store-student', function (user $user) {
